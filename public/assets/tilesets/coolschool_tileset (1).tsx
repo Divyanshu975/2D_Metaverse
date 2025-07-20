90aeceee042eb5891720e1184d93782cf5649abd
Divyanshu975 <divyanshubk2003@gmail.com>
2025-07-21 03:41:26 +0530

    Tiled map added

---
+++ public/assets/tilesets/coolschool_tileset (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="coolschool_tileset (1)" tilewidth="32" tileheight="32" tilecount="864" columns="24">
+ <image source="../coolschool_tileset (1).png" width="768" height="1152"/>
+</tileset>
---
+++ public/assets/tilesets/coolschool_tileset-1.png.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="coolschool_tileset-1.png" tilewidth="32" tileheight="32" tilecount="864" columns="24">
+ <image source="../coolschool_tileset-1.png.png" width="768" height="1152"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
